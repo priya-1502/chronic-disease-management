53662017833be21003c2716020fb17b85d0b5ae2
Shanmugam Manimaran <shanmugammanimaran@Shanmugams-MacBook-Pro.local>
2025-03-08 11:51:15 +0530

    feat:added router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import './index.css';
 import App from './App';
+import About from './components/About/About';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
